ced296e08c386cd17dd2dee65fa36afb90820322
Daniel Guarecuco <daniel.guarecuco@gmail.com>
2022-05-03 19:20:22 +0200

    Adding some USERS API

--- code/server/dao/UserDAO.js
+++ code/server/dao/UserDAO.js
@@ -5,9 +5,9 @@ class UserDAO{
             if(err) throw err
         })
     }
-    newTableName(){
+    newTableUsers(){
         return new Promise((resolve, reject) => {
-            const sql = 'CREATE TABLE IF NOT EXISTS NAMES(ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME VARCHAR, SURNAME VARCHAR)';
+            const sql = 'CREATE TABLE IF NOT EXISTS USERS(ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME VARCHAR, SURNAME VARCHAR, EMAIL VARCHAR, PASSWORD VARCHAR, TYPE VARCHAR)';
             this.db.run(sql, (err) => {
                 if(err){
                     reject(err);
@@ -18,34 +18,118 @@ class UserDAO{
         })
     }
 
+    getLoggedUsers() {
+        return new Promise((resolve, reject) => {
+            const sql = 'SELECT * FROM USERS'           //Add condition to check if online
+            this.db.all(sql, [], (err, rows) => {
+                if(err){
+                    reject(err);
+                    return;
+                }
+                const names = rows.map((r) => (
+                    {
+                        id: r.ID,
+                        name: r.NAME,
+                        surname: r.SURNAME,
+                        email: r.EMAIL,
+                        type: r.TYPE
+                    }
+                    
+                ));
+                resolve(names)
+            })
+        })
+    }
+
+    getStoredSuppliers() {
+        return new Promise((resolve, reject) => {
+            const sql = 'SELECT * FROM USERS WHERE TYPE = "supplier"'
+            this.db.all(sql, [], (err, rows) => {
+                if(err){
+                    reject(err);
+                    return;
+                }
+                const names = rows.map((r) => (
+                    {
+                        id: r.ID,
+                        name: r.NAME,
+                        surname: r.SURNAME,
+                        email: r.EMAIL,
+                        type: r.TYPE
+                    }
+                    
+                ));
+                resolve(names)
+            })
+        })
+    }
+
     getStoredUsers() {
         return new Promise((resolve, reject) => {
-            resolve([{id: 2,
-                name: 'ale',
-                surname: 'carachino'}])
-            //const sql = 'SELECT * FROM USERS'
-            //this.db.all(sql, [], (err, rows) => {
-            //    if(err){
-            //        reject(err);
-            //        return;
-             //   }
-             //   const names = rows.map((r) => (
-             //       {
-             //           id: r.ID,
-            //            name: r.NAME,
-             //           surname: r.SURNAME
-             //       }
+            const sql = 'SELECT * FROM USERS'
+            this.db.all(sql, [], (err, rows) => {
+                if(err){
+                    reject(err);
+                    return;
+                }
+                const names = rows.map((r) => (
+                    {
+                        id: r.ID,
+                        name: r.NAME,
+                        surname: r.SURNAME,
+                        email: r.EMAIL,
+                        type: r.TYPE
+                    }
                     
-             //   ));
-            //    resolve(names)
-            //})
+                ));
+                resolve(names)
+            })
+        })
+    }
+
+    getStoredUsersWithoutManagers() {
+        return new Promise((resolve, reject) => {
+            const sql = 'SELECT * FROM USERS WHERE TYPE != "manager"'
+            this.db.all(sql, [], (err, rows) => {
+                if(err){
+                    reject(err);
+                    return;
+                }
+                const names = rows.map((r) => (
+                    {
+                        id: r.ID,
+                        name: r.NAME,
+                        surname: r.SURNAME,
+                        email: r.EMAIL,
+                        type: r.TYPE
+                    }
+                    
+                ));
+                resolve(names)
+            })
+        })
+    }
+
+    checkIfStored(data){
+        return new Promise((resolve, reject) => {
+            const sql = 'SELECT COUNT(EMAIL) as COUNT FROM USERS WHERE EMAIL = ? AND TYPE = ?'
+            this.db.all(sql, [data.username, data.type] , (err, rows) => {
+                if(err){
+                    reject(err);
+                    return;
+                }
+                const count = rows.map((r) => (
+                    r.COUNT
+                ));
+                resolve(count)
+            })
         })
     }
 
     storeUser(data){
         return new Promise((resolve, reject) => {
-            const sql = 'INSERT INTO NAMES(NAME, SURNAME) VALUES (?,?)'
-            this.db.run(sql, [data.name, data.surname] , (err) => {
+            const sql = 'INSERT INTO USERS(NAME, SURNAME, EMAIL, PASSWORD, TYPE) VALUES (?,?,?,?,?)'
+            this.db.run(sql, [data.name, data.surname, data.username, data.password, data.type] , (err) => {
                 if(err){
                     reject(err);
                     return;
--- code/server/routesModule/UserRoutes.js
+++ code/server/routesModule/UserRoutes.js
@@ -3,10 +3,36 @@ const UserDAO = require('../dao/UserDAO.js')
 const db = new UserDAO('EzWh')
 
 const router = express.Router()
+router.use(express.json());
 
+//GET /api/userinfo
+router.get('/api/userinfo', async (req,res)=>{
+    try{
+        const users = await db.getLoggedUsers();
+        return res.status(200).json(users);
+    }
+    catch(err){
+        res.status(500).end();
+    }
+  
+  }); 
+
+//GET /api/suppliers
+router.get('/api/suppliers', async (req,res)=>{
+    try{
+        const users = await db.getStoredSuppliers();
+        return res.status(200).json(users);
+    }
+    catch(err){
+        res.status(500).end();
+    }
+  
+  }); 
+
+//GET /api/users
 router.get('/api/users', async (req,res)=>{
     try{
-        const users = await db.getStoredUsers();
+        const users = await db.getStoredUsersWithoutManagers();
         return res.status(200).json(users);
     }
     catch(err){
@@ -14,5 +40,40 @@ router.get('/api/users', async (req,res)=>{
     }
     
   }); 
+  
+//POST /api/newUser
+router.post('/api/newUser', async (req,res)=>{
+    try{
+        //Check if body is empty
+        if (Object.keys(req.body).length === 0) {
+            return res.status(422).json({error: `Empty body request`});
+          }
+        let user = req.body;
+          //Check if any field is empty
+        if (user === undefined || user.name === undefined || user.surname === undefined ||  
+            user.username === undefined || user.password === undefined || user.type === undefined || 
+            user.name == '' || user.surname == '' || user.username == '' || user.password == '' || user.type == '') {
+                return res.status(422).json({error: `Invalid user data`});
+        }
+        //Check type is correct
+        if (user.type !== 'customer' && user.type !== 'qualityEmployee' && user.type !== 'clerk' && 
+            user.type !== 'deliveryEmployee' && user.type !== 'supplier') {
+            return res.status(422).json({error: `Invalid user data`});
+        }
+        await db.newTableUsers();
 
-module.exports = router
+        //Check if user exist
+        let count = await db.checkIfStored(user);
+        if (count == 0){
+            db.storeUser(user);
+            return res.status(201).end(); 
+        }   
+        return res.status(409).json({error: `User already exists`});
+    }
+    catch(err){
+        res.status(500).end();
+    }
+ 
+  }); 
+
+module.exports = router;
