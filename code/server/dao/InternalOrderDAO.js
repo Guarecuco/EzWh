eb5d09c8e631d16011b900e4a0710f2d41767d92
Daniel Guarecuco <daniel.guarecuco@gmail.com>
2022-05-06 14:47:23 +0200

    Internal orders

--- code/server/dao/InternalOrderDAO.js
+++ code/server/dao/InternalOrderDAO.js
@@ -6,6 +6,99 @@ class InternalOrderDAO{
         })
     }
 
-}
+    newTableInternalOrders(){
+        return new Promise((resolve, reject) => {
+            const sql = 'CREATE TABLE IF NOT EXISTS INTERNAL_ORDERS(ID INTEGER PRIMARY KEY AUTOINCREMENT, ISSUEDATE DATE, STATE VARCHAR, CUSTOMERID INTEGER)';
+            this.db.run(sql, (err) => {
+                if(err){
+                    reject(err);
+                    return;
+                }
+                resolve(this.lastID)
+            })
+        })
+    }
+
+    newTableInternalOrdersProducts(){
+        return new Promise((resolve, reject) => {
+            const sql = 'CREATE TABLE IF NOT EXISTS INTERNAL_ORDERS_PRODUCTS(ID INTEGER PRIMARY KEY AUTOINCREMENT, SKUID INTEGER, DESCRIPTION VARCHAR, PRICE FLOAT, QTY INTEGER, RFID VARCHAR, ORDERID INTEGER)';
+            this.db.run(sql, (err) => {
+                if(err){
+                    reject(err);
+                    return;
+                }
+                resolve(this.lastID)
+            })
+        })
+    }
+
+    storeInternalOrder(data){
+        return new Promise((resolve, reject) => {
+            const sql = 'INSERT INTO INTERNAL_ORDERS(ISSUEDATE,STATE,CUSTOMERID) VALUES (?,?,?) RETURNING ID'
+            this.db.run(sql, [data.issueDate, data.state, data.customerId] , function (err) {
+                if(err){
+                    reject(err);
+                    return;
+                }
+                resolve(this.lastID)
+            })
+        })
+    }
+
+    storeInternalOrderProducts(data){
+        return new Promise((resolve, reject) => {
+            const sql = 'INSERT INTO INTERNAL_ORDERS_PRODUCTS(SKUID, DESCRIPTION, PRICE, QTY, ORDERID) VALUES (?,?,?,?,?)'
+            this.db.run(sql, [data.SKUId, data.description, data.price, data.qty, data.orderId] , (err) => {
+                if(err){
+                    reject(err);
+                    return;
+                }
+                resolve(this.lastID)
+            })
+        })
+    }
+
+    checkIfOrderExists(data){
+        return new Promise((resolve, reject) => {
+            const sql = 'SELECT COUNT(ID) as COUNT FROM INTERNAL_ORDERS WHERE ID = ?'
+            this.db.all(sql, [data.orderId] , (err, rows) => {
+                if(err){
+                    reject(err);
+                    return;
+                }
+                const count = rows.map((r) => (
+                    r.COUNT
+                ));
+                resolve(count)
+            })
+        })
+    }
+
+    updateInternalOrder(data){
+        return new Promise((resolve, reject) => {
+            const sql = 'UPDATE INTERNAL_ORDERS SET STATE = ? WHERE ID = ?'
+            this.db.run(sql, [data.newState, data.orderId] , (err) => {
+                if(err){
+                    reject(err);
+                    return;
+                }
+                resolve(this.lastID)
+            })
+        })
+    }
+
+    updateInternalOrderProducts(data){
+        return new Promise((resolve, reject) => {
+            const sql = 'UPDATE INTERNAL_ORDERS_PRODUCTS SET RFID = ? WHERE ORDERID = ? AND SKUId = ?'
+            this.db.run(sql, [data.RFID, data.orderId, data.SKUId] , (err) => {
+                if(err){
+                    reject(err);
+                    return;
+                }
+                resolve(this.lastID)
+            })
+        })
+    }
+};
 
 module.exports = InternalOrderDAO;
--- code/server/routesModule/InternalOrderRoutes.js
+++ code/server/routesModule/InternalOrderRoutes.js
@@ -5,4 +5,125 @@ const db = new InternalOrderDAO('EzWh')
 const router = express.Router()
 router.use(express.json());
 
+const date_regex = /^(19|20)\d{2}\/([1-9]|1[0-2])\/([1-9]|1\d|2\d|3[01]) (0[0-9]|1[0-9]|2[0-3]):(0[0-9]|1[0-9]|2[0-9]|3[0-9]|4[0-9]|5[0-9]|)$/;
+
+//GET /api/internalOrders
+router.get('/api/internalOrders', async (req,res)=>{
+    try{
+        const users = await db.getStoredSuppliers();
+        return res.status(200).json(users);
+    }
+    catch(err){
+        res.status(500).end();
+    }
+  
+}); 
+//GET /api/internalOrdersIssued
+
+//GET /api/internalOrdersAccepted
+
+//GET /api/internalOrders/:id
+
+
+//POST /api/internalOrders
+router.post('/api/internalOrders', async (req,res)=>{
+    try{
+        //Check if body is empty
+        if (Object.keys(req.body).length === 0) {
+            return res.status(422).json({error: `Empty body request`});
+            }
+        let order = req.body;
+        //Check if any field is empty
+        if (order === undefined || order.issueDate === undefined || order.products === undefined ||  
+            order.customerId === undefined || order.issueDate == '' || order.products == '' || 
+            order.customerId == '' ) {
+                return res.status(422).json({error: `Invalid data`});
+        }
+        //Check product is not empty
+
+        //Check date format
+        if(!(date_regex.test(order.issueDate))){
+            return res.status(422).json({error: `Invalid date`});
+        }
+
+        //Store order in DB
+        await db.newTableInternalOrders();
+        await db.newTableInternalOrdersProducts();
+
+        order.state = "ISSUED";
+        order.orderId = await db.storeInternalOrder(order);
+        
+        for (var i=0; i<order.products.length; i++){
+            let product = {
+                SKUId : order.products[i].SKUId,
+                description: order.products[i].description,
+                price: order.products[i].price,
+                qty: order.products[i].qty,
+                orderId : order.orderId
+            }
+
+            await db.storeInternalOrderProducts(product);
+        }
+        
+        return res.status(200).end();
+    }
+    catch(err){
+        res.status(500).end();
+    }
+}); 
+
+//PUT /api/internalOrders/:id
+router.put('/api/internalOrders/:id', async (req,res)=>{
+    try{
+
+        //Check if body is empty
+        if (Object.keys(req.body).length === 0) {
+            return res.status(422).json({error: `Empty body request`});
+        }
+        
+        let order = req.body;
+        order.orderId = req.params.id;
+
+        //Check if state is valid
+        if (order.newState !== 'ISSUED' && order.newState !== 'ACCEPTED' && order.newState !== 'REFUSED' && 
+            order.newState !== 'CANCELED' && order.newState !== 'COMPLETED') {
+                return res.status(422).json({error: `Invalid state`});
+        }
+
+        //Check if orderId exist
+        let count = await db.checkIfOrderExists(order);
+        if (count == 0){
+            return res.status(404).end();
+        }
+
+        //Update
+        await db.updateInternalOrder(order);
+
+        //Update products when COMPLETED
+        if (order.newState == 'COMPLETED'){
+            //Check product is not empty
+            if(order.products === undefined || order.products == ''){
+                return res.status(422).json({error: `Invalid body request`});  
+            }
+           
+            for (var i=0; i<order.products.length; i++){
+                let product = {
+                    SKUId : order.products[i].SKUId,
+                    RFID : order.products[i].RFID,
+                    orderId: order.orderId
+                }
+    
+                await db.updateInternalOrderProducts(product);
+            }         
+        }
+        
+        return res.status(200).end();
+    }
+    catch(err){
+        res.status(503).end();
+    }
+}); 
+
+//DELETE /api/internalOrders/:id
+
 module.exports = router;
