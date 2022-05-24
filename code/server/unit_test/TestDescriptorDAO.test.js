8140ab40c0d682c3b76b7705507e28c2f2b73bd2
ZolTorino <79834667+ZolTorino@users.noreply.github.com>
2022-05-24 17:36:18 +0200

    tests

--- code/server/unit_test/TestDescriptorDAO.test.js
+++ code/server/unit_test/TestDescriptorDAO.test.js
@@ -1,34 +1,111 @@
-const testDao = require('../dao/TestDescriptorDAO');
-
-describe('testDao', () => {
-    beforeAll(async () => {
-        await testDao.deleteAllTests();
-    });
-
-    test('delete db', async () => {
-        var res = await testDao.getTestsDescriptors();
-        expect(res.length).toStrictEqual(0);
-    });
-    let atest = {
-        name: 'rig everything',
-        procedureDescription:  'What a beautiful test',
-        idSKU: 1
-    }
-    testAddTest(atest);
-});
+const testDAO = require('../dao/TestDescriptorDAO');
+const db = new testDAO('EzWh.db');
 
 function testAddTest(input) {
-    test('create new test', async () => {
+    test('Create new test', async () => {
         
-        await testDao.addTest(input);
+        await db.addTest(input);
         
-        var res = await testDao.getAllUsers();
-        expect(res.length).toStrictEqual(1);
-        
-        res = await testDao.getTestDescriptor(1);
+        let res = await db.findTestName(input.name);
 
-        expect(res.name).toStrictEqual(test.name);
-        expect(res.procedureDescription).toStrictEqual(test.procedureDescription);
-        expect(res.idSKU).toStrictEqual(test.idSKU);
+        expect(res[0].name).toStrictEqual(input.name);
+        expect(res[0].procedureDescription).toStrictEqual(input.procedureDescription);
+        expect(res[0].idSKU).toStrictEqual(input.idSKU);
+        
     });
 }
+
+function testEditTest(input) {
+    test('Edit existing test', async () => {
+
+        await db.updateTest(input.id)
+        let res = await db.findTestId(input.id);
+
+        expect(res[0].name).toStrictEqual(input.name);
+        expect(res[0].procedureDescription).toStrictEqual(input.procedureDescription);
+        expect(res[0].idSKU).toStrictEqual(input.idSKU);
+    });
+}
+
+function testGetTests(input) {
+    test('Retrieving tests', async () => {
+
+        let res = await db.getTestsDescriptors();
+
+        if (res[0] !== undefined){
+            expect(res[0].id).toStrictEqual(input.id);
+            expect(res[0].name).toStrictEqual(input.name);
+            expect(res[0].procedureDescription).toStrictEqual(input.procedureDescription);
+            expect(res[0].idSKU).toStrictEqual(input.idSKU);
+        }
+        else{
+            expect(res[0]).toStrictEqual([]);
+        }
+    });
+}
+
+function testGetTest(input) {
+    test('Retrieving test', async () => {
+
+        await db.updateTest(input.id)
+        let res = await db.findTestId(input.id);
+        if (res[0] !== undefined){
+            expect(res[0].id).toStrictEqual(input.id);
+            expect(res[0].name).toStrictEqual(input.name);
+            expect(res[0].procedureDescription).toStrictEqual(input.procedureDescription);
+            expect(res[0].idSKU).toStrictEqual(input.idSKU);
+        }
+    });
+}
+
+
+
+function testDeleteTest(input) {
+    test('Delete existing test', async () => {
+        
+        await db.deleteTest(input);
+        
+        let res = await db.findTestId(input);
+
+        expect(res[0]).toStrictEqual(undefined);
+    });
+}
+
+
+
+//************************************************************************** */
+//Calling test functions
+describe('Test TestDescriptor DAO', () => {
+    beforeAll(async () => {
+        await db.dropTestsTable();
+        await db.newTableTests();
+    });
+
+    let test={
+        name: "prova",
+        procedureDescription: "dei test descriptor",
+        idSKU: 1
+    }
+    testAddTest(test)
+    /*Get user, should be the same as just added
+    testGetUsers({username: "user1@ezwh.com",name: "John",surname : "Smith",type : "customer"});
+    //Get users excluding manager
+    testGetUsersWithoutManagers({username: "user1@ezwh.com",name: "John",surname : "Smith",type : "customer"});
+    //Get user using email and type
+    testGetUserByEmailType({username: "user1@ezwh.com",name: "John",surname : "Smith",password : "testpassword",type : "customer"});
+    //Check if user is stored
+    testCheckStored({username: "user1@ezwh.com",type : "customer",count:1})
+    
+    //Edit User to supplier
+    testEditUser({username: "user1@ezwh.com",type : "customer", newType: "supplier"});
+    //Get user, should be the same as just edited
+    testGetUsers({username: "user1@ezwh.com",name: "John",surname : "Smith",type : "supplier"});
+    //Get suppliers, should be the same as just edited
+    testGetSuppliers({username: "user1@ezwh.com",name: "John",surname : "Smith"});
+    
+    //Delete user (supplier)
+    testDeleteUser({username: "user1@ezwh.com",type : "supplier"});
+    //Get users, should be empty
+    testGetUsers(undefined);
+   */
+});
