3a426fd64bc159117c3884a7840c6cf93ef0b31b
Daniel Guarecuco <daniel.guarecuco@gmail.com>
2022-05-24 19:32:59 +0200

    Fixing internalOrder cross-reference. Update reprt

--- code/server/test/testInternalOrderRoutes.js
+++ code/server/test/testInternalOrderRoutes.js
@@ -123,25 +123,47 @@ function deleteInternalOrder(expectedHTTPStatus,id){
     })
 }
 
+
+//*********************************************************************************************** */
+//Some data (SKU) is needed to issue new orders
+before(function (done) {
+    agent.delete('/api/skus/').then(res => {
+        agent.post('/api/sku')
+            .send({"description" : "SKU1","weight" : 100,"volume" : 50,"notes" : "Testing SKU","price" : 10.99,"availableQuantity" : 100 })
+            .then(function (res) {
+            })
+        agent.post('/api/sku')
+            .send({"description" : "SKU2","weight" : 50,"volume" : 100,"notes" : "Testing SKU 2","price" : 700,"availableQuantity" : 200 })
+            .then(function (res) {
+            })
+        agent.post('/api/sku')
+            .send({"description" : "SKU3","weight" : 50,"volume" : 100,"notes" : "Testing SKU 3","price" : 20,"availableQuantity" : 50 })
+            .then(function (res) {
+                done();
+            })
+    })
+});
+
+
 //*********************************************************************************************** */
 //Call to testing functions
 
 describe('Test internalOrder APIs', () => {
     dropInternalOrders(204);
 
-    newInternalOrder(201,"2021/11/29 09:33", [{"SKUId":1,"description":"ACME Laptop","price":1000,"qty":3},
-        {"SKUId":2,"description":"ACME Mouse","price":50.99,"qty":3}], 1);    //New order with 2 produtcs
-    newInternalOrder(201,"2022/05/10 09:33", [{"SKUId":50,"description":"ACME phone","price":800,"qty":3}], 1);    //New order with 1 product
-    newInternalOrder(201,"2022/05/15 10:45", [{"SKUId":245,"description":"ACME pencil","price":1,"qty":500},
-        {"SKUId":246,"description":"ACME pen","price":1,"qty":500}], 2);    //New order with 2 produtcs
-    newInternalOrder(201,"2022/07/10 15:39", [{"SKUId":87,"description":"ACME Y","price":1,"qty":89}], 3);    //New order with 1 product
-    newInternalOrder(201,"2022/07/12 06:23", [{"SKUId":96,"description":"ACME Z","price":1,"qty":96}], 4);    //New order with 1 product
+    newInternalOrder(201,"2021/11/29 09:33", [{"SKUId":1,"description":"SKU1","price":10.99,"qty":3},
+                                             {"SKUId":2,"description":"SKU2","price":700,"qty":50}], 1);    //New order with 2 produtcs
+    newInternalOrder(201,"2022/05/10 09:33", [{"SKUId":3,"description":"SKU3","price":20,"qty":1}], 1);    //New order with 1 product
+    newInternalOrder(201,"2022/05/15 10:45", [{"SKUId":1,"description":"SKU1","price":10.99,"qty":5},
+        {"SKUId":3,"description":"SKU3","price":20,"qty":7}], 2);    //New order with 2 produtcs
+    newInternalOrder(201,"2022/07/10 15:39", [{"SKUId":1,"description":"SKU1","price":10.99,"qty":8}], 4);    //New order with 1 product
+    newInternalOrder(201,"2022/07/12 06:23", [{"SKUId":3,"description":"SKU3","price":20,"qty":8}], 4);    //SKUId doesnt exist
     newInternalOrder(422,"2022/05/10 09:33", "", 1);    //Empty product array
     newInternalOrder(422,"05/10/2022 09:33", "", 1);    //Wrong date format
     newInternalOrder(422,"2022/05/10 09:33", [{"SKUId":"abc","description":"ACME phone","price":800,"qty":3}], 1);    //Wrong skuid - not an integer
-    newInternalOrder(422,"2022/05/10 09:33", [{"SKUId":40,"description":"ACME phone","price":800,"qty":3.5}], 1);   //Wrong qty - not an integer
-    newInternalOrder(422,"2022/05/10 09:33", [{"SKUId":40,"description":"ACME phone","price":800,"qty":3}], 1.3);    //Wrong customerID - not an integer
-    newInternalOrder(422,"2022/05/10 09:33", [{"SKUId":40,"description":"ACME phone","price":"price","qty":3}], 1);    //Wrong price, not number
+    newInternalOrder(422,"2022/05/10 09:33", [{"SKUId":1,"description":"SKU1","price":10.99,"qty":3.5}], 1);   //Wrong qty - not an integer
+    newInternalOrder(422,"2022/05/10 09:33", [{"SKUId":1,"description":"SKU1","price":10.99,"qty":3}], 1.3);    //Wrong customerID - not an integer
+    newInternalOrder(422,"2022/05/10 09:33", [{"SKUId":1,"description":"SKU1","price":"price","qty":3}], 1);    //Wrong price, not number
     newInternalOrder(422);    //Empty body
 
     editInternalOrder(200,1,"COMPLETED",[{"SKUId":1,"RFID":"12345678901234567890123456789016"},
@@ -160,37 +182,37 @@ describe('Test internalOrder APIs', () => {
             "id":1,
             "issueDate":"2021/11/29 09:33",
             "state": "COMPLETED",
-            "products":[{"SKUId":1,"description":"ACME Laptop","price":1000,"RFID":"12345678901234567890123456789016"},
-                        {"SKUId":2,"description":"ACME Mouse","price":50.99,"RFID":"12345678901234567890123456789038"}],
+            "products":[{"SKUId":1,"description":"SKU1","price":10.99,"RFID":"12345678901234567890123456789016"},
+                        {"SKUId":2,"description":"SKU2","price":700,"RFID":"12345678901234567890123456789038"}],
             "customerId" : 1
         },
         {
             "id":2,
             "issueDate":"2022/05/10 09:33",
             "state": "ACCEPTED",
-            "products": [{"SKUId":50,"description":"ACME phone","price":800,"qty":3}],
+            "products": [{"SKUId":3,"description":"SKU3","price":20,"qty":1}],
             "customerId" : 1
         },
         {
             "id":3,
             "issueDate":"2022/05/15 10:45",
             "state": "REFUSED",
-            "products": [{"SKUId":245,"description":"ACME pencil","price":1,"qty":500},
-            {"SKUId":246,"description":"ACME pen","price":1,"qty":500}],
+            "products": [{"SKUId":1,"description":"SKU1","price":10.99,"qty":5},
+            {"SKUId":3,"description":"SKU3","price":20,"qty":7}],
             "customerId" : 2
         },
         {
             "id":4,
             "issueDate":"2022/07/10 15:39",
             "state": "CANCELED",
-            "products": [{"SKUId":87,"description":"ACME Y","price":1,"qty":89}],
-            "customerId" : 3
+            "products": [{"SKUId":1,"description":"SKU1","price":10.99,"qty":8}],
+            "customerId" : 4
         },
         {
             "id":5,
             "issueDate":"2022/07/12 06:23",
             "state": "ISSUED",
-            "products": [{"SKUId":96,"description":"ACME Z","price":1,"qty":96}],
+            "products": [{"SKUId":3,"description":"SKU3","price":20,"qty":8}],
             "customerId" : 4
         }
     ]);
@@ -200,7 +222,7 @@ describe('Test internalOrder APIs', () => {
             "id":5,
             "issueDate":"2022/07/12 06:23",
             "state": "ISSUED",
-            "products": [{"SKUId":96,"description":"ACME Z","price":1,"qty":96}],
+            "products": [{"SKUId":3,"description":"SKU3","price":20,"qty":8}],
             "customerId" : 4
         }
     ]);
@@ -210,17 +232,17 @@ describe('Test internalOrder APIs', () => {
             "id":2,
             "issueDate":"2022/05/10 09:33",
             "state": "ACCEPTED",
-            "products": [{"SKUId":50,"description":"ACME phone","price":800,"qty":3}],
+            "products": [{"SKUId":3,"description":"SKU3","price":20,"qty":1}],
             "customerId" : 1
         }
     ]);
     
     getinternalOrdersById(200,{"id":1,"issueDate":"2021/11/29 09:33","state": "COMPLETED",
-    "products":[{"SKUId":1,"description":"ACME Laptop","price":1000,"RFID":"12345678901234567890123456789016"},
-                {"SKUId":2,"description":"ACME Mouse","price":50.99,"RFID":"12345678901234567890123456789038"}],
+    "products":[{"SKUId":1,"description":"SKU1","price":10.99,"RFID":"12345678901234567890123456789016"},
+                {"SKUId":2,"description":"SKU2","price":700,"RFID":"12345678901234567890123456789038"}],
     "customerId" : 1},1);                                                                                   //OK
     getinternalOrdersById(200,{"id":4,"issueDate":"2022/07/10 15:39","state": "CANCELED",
-        "products": [{"SKUId":87,"description":"ACME Y","price":1,"qty":89}], "customerId" : 3},4);         //OK
+        "products": [{"SKUId":1,"description":"SKU1","price":10.99,"qty":8}], "customerId" : 4},4);         //OK
     getinternalOrdersById(404,"",96);         //order with ID = 96 doesnt exist
 
     deleteInternalOrder(204,1)  //OK
